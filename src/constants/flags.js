f09d6f8cc9ef21c08eab172a8b0110951733bb56
Denis Lupookov <denislupookov@gmail.com>
2026-04-16 13:49:43 +0800

    Added design v2 FF for mobile

--- src/constants/flags.js
+++ src/constants/flags.js
@@ -6,4 +6,5 @@ export const SAVE_CREDENTIALS_AFTER_LOGIN_ENABLED = false
 export const DELETE_VAULT_ENABLED = false
 export const AUTHENTICATOR_ENABLED = false
 export const DESIGN_VERSION = 1
+export const MOBILE_DESIGN_VERSION = 2
 export const UNSUPPORTED = true
